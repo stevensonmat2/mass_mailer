7be4d0dd3df9b9cf0d2e0b71c3e77f8cf3d7c542
stevensonmat2 <stevensonmat2@gmail.com>
2022-10-13 20:11:56 -0700

    update

--- mailer.py
+++ mailer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from this import d
 from dotenv import load_dotenv
 import smtplib
 import os
@@ -19,6 +20,8 @@ TEST_RECEIVE_EMAIL = os.environ.get("TEST_RECEIVE_EMAIL")
 volunteers = []
 date = datetime(args.year, args.month, args.day)
 weekday = date.strftime("%A")
+month = date.month
+day = date.day
 
 # creates SMTP session
 s = smtplib.SMTP("smtp.gmail.com", 587)
@@ -40,24 +43,30 @@ with open("ccm.csv") as csv_file:
 
     print(f"Processed {line_count} lines.")
 
-for volunteer in volunteers[:1]:
-    to = "To: {}\r\nSubject: {}\r\n\r\n".format(
-        "matstev2@pdx.edu", f"free this {weekday}?"
-    )
+for volunteer in volunteers:
     name = volunteer.get("name")
     email = volunteer.get("email")
+    to = "To: {}\r\nSubject: {}\r\n\r\n".format(
+        email, f"free this {weekday}?"
+    )
     message = (
         f"{to}"
-        f"Hello {name}! \n\n"
+        f"Hello {name}! Welcome to the Climate Changemakers community.\n\n"
         f"I'm Matt Stevenson, a Climate Change Makers ambassador, and I'm reaching out "
-        f"to see if you would like to join me for an hour of action on this coming {weekday}, "
-        f"{date.month}/{date.day}.\n\n"
-        f"Hope to see you there!\n\n"
-        f"Matt Stevenson"
+        f"to see if I can answer any questions you might have. "
+        f"I would also like to invite you "
+        f"to join me this {weekday} {month}/{day} for an hour of action! "
+        f"As we head towards the midterms, "
+        f"we would love to have you join and increase our impact. "
+        f"Now is the perfect time to really make your action count!\n\n"
+        f"Link to the hour of action: https://lu.ma/4ykcvbou \n\n"
+        f"Let me know if you have any questions, and hope to see you there!\n\n"
+        f"Matt Stevenson\n\n"
+        f"https://www.climatechangemakers.org/action-hub"
     )
     print(message)
 
-    s.sendmail(TEST_SEND_EMAIL, [TEST_RECEIVE_EMAIL], message)
+    s.sendmail(TEST_SEND_EMAIL, [email], message)
 
 # terminating the session
 s.quit()
